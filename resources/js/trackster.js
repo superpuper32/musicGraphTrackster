dba2d25565a4255e10493343798ba35c84a391c4
superpuper32 <pochitalin_m@mail.ru>
2018-01-26 00:10:07 +0300

    Completed query the Last.fm API

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,10 +1,15 @@
+var Trackster = {};
+const API_KEY = 'e396df6cd5b772d53b26ae1c0deb64e2';
+
 $(document).ready(() => {
   $('#search-btn').on('click', () => {
     Trackster.searchTracksByTitle($('#search-input').val());
   })
+
+
 });
 
-var Trackster = {};
+
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
@@ -19,5 +24,11 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-
+  $.ajax({
+    url: "http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + title + "&api_key=" + API_KEY + "&format=json",
+    success: console.log('response')
+  });
 };
+
+
+/* Shared Secret 58e90f41ca9e103c47eb45149a27dc55 */
